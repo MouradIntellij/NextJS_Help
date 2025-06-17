dc656d64360dcc18e323a886afb318a61f11a65c
MouradWPF <139013207+MouradWPF@users.noreply.github.com>
2025-06-17 00:15:15 -0400

    Corrige l'export manquant dans la page blog

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -11,3 +11,17 @@
 //     </div>
 //   );
 // }
+
+// src/app/[locale]/blog/page.tsx
+
+import React from 'react';
+
+export default function BlogPage() {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Bienvenue sur le blog !</p>
+    </div>
+  );
+}
+
