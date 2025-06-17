6cc8d98ca1cafc1dc1f0d2ae433706fbb95c4216
MouradWPF <139013207+MouradWPF@users.noreply.github.com>
2025-06-17 01:10:28 -0400

    Ajout du fichier page.tsx dans src/app/contact/

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+// src/app/contact/page.tsx
+import FormSection from '../components/FormSection/FormSection';
+export default function ContactPage() {
+  return (
+    <main>
+      <FormSection />
+    </main>
+  );
+}
